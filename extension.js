9459f03021271868079c694721fee2048a7e26e2
Erick Tucto <erick.tucto@outlook.com>
2018-08-22 17:53:52 -0500

    Change syntax ES6

--- extension.js
+++ extension.js
@@ -6,39 +6,49 @@ const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 
+let app;
+class IconActivities {
+    constructor() {
+        this._activities = Main.panel.statusArea['activities'];
+        this._build({ icon: Me.path + "/icons/logo-ubuntu.svg" });
+    }
+    _build(prop) {
+        this._activities.actor.destroy_all_children();
+        this._activities._label = _getLabel(prop);
+        this._activities.actor.add_actor(this._activities._label);
+        this._activities.actor.label_actor = this._activities._label;
+    }
+    _setLabel(prop) {
+        if (prop.text) {
+            return new St.Label({
+                text: prop.text,
+                y_align: Clutter.ActorAlign.CENTER
+            });
+        } else if (prop.icon) {
+            let gicon = Gio.icon_new_for_string(prop.icon);
+            let icon = new St.Icon({
+                gicon: gicon,
+                style_class: 'iconActivities'
+            });
+            icon.set_size(32, 32);
+            return icon;
+        }
+    }
+    destroy() {
+        let text = _("Activities");
+        this._build({ text: text });
+    }
+}
+
+
 function init(){
 }
 
 function enable(){
-    _changeActivities({ icon: Me.path + "/icons/logo-ubuntu.svg" });
+    app = new IconActivities();
 }
 
 function disable() {
-    let text = _("Activities");
-    _changeActivities({ text: text });
-}
-
-function _changeActivities(prop) {
-    let Activities = Main.panel.statusArea['activities'];
-    Activities.actor.destroy_all_children();
-    Activities._label = _getLabel(prop);
-    Activities.actor.add_actor(Activities._label);
-    Activities.actor.label_actor = Activities._label;
-}
-
-function _getLabel(prop) {
-    if (prop.text) {
-        return new St.Label({
-            text: prop.text,
-            y_align: Clutter.ActorAlign.CENTER
-        });
-    } else if (prop.icon) {
-        let gicon = Gio.icon_new_for_string(prop.icon);
-        let icon = new St.Icon({
-            gicon: gicon,
-            style_class: 'iconActivities'
-        });
-        icon.set_size(35, 35);
-        return icon;
-    }
+    app.destroy();
+    app = undefined;
 }
