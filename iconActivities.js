406a96edb02d977ba300acb19c7df3950c3fe659
Erick Tucto <erick.tucto@outlook.com>
2018-08-24 11:37:37 -0500

    Create Icon class

--- iconActivities.js
+++ iconActivities.js
@@ -1,41 +1,22 @@
-imports.gi.versions.St = "1.0";
-
-const St = imports.gi.St;
-const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
-const Clutter = imports.gi.Clutter;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Icon = Me.imports.icon;
 
 class IconActivities {
     constructor() {
         this._activities = Main.panel.statusArea['activities'];
-        this._build({ icon: Me.path + "/icons/logo-ubuntu.svg" });
+        this._icon = new Icon.Icon({ icon: Me.path + "/icons/logo-ubuntu.svg" });
+        this._build(this._icon);
     }
-    _build(prop) {
+    _build(icon) {
         this._activities.actor.destroy_all_children();
-        this._activities._label = this._setLabel(prop);
+        this._activities._label = icon.getLabel();
         this._activities.actor.add_actor(this._activities._label);
         this._activities.actor.label_actor = this._activities._label;
     }
-    _setLabel(prop) {
-        if (prop.text) {
-            return new St.Label({
-                text: prop.text,
-                y_align: Clutter.ActorAlign.CENTER
-            });
-        } else if (prop.icon) {
-            let gicon = Gio.icon_new_for_string(prop.icon);
-            let icon = new St.Icon({
-                gicon: gicon,
-                style_class: 'iconActivities'
-            });
-            icon.set_size(32, 32);
-            return icon;
-        }
-    }
     destroy() {
         let text = _("Activities");
-        this._build({ text: text });
+        this._build(new Icon.Icon({ text: text }));
     }
 }
